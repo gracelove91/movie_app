a96acc86dbd038b1eb26fe7e39749769169def4b
gracelove <govlmo91@gmail.com>
2020-02-27 01:40:59 +0900

    컴포넌트 다양하게 생성할 때 map 연습

--- src/App.js
+++ src/App.js
@@ -4,27 +4,33 @@ import React from 'react';
 function Food({name, picture}) {
     return <div>
         <h2> I Like {name}</h2>
-        <img src={picture} />
+        <img src={picture}  alt={name}/>
     </div>
 }
 
 const foodsILike = [
     {
+        id: 1,
         name: "Kimchi",
         image: "http://assets.epicurious.com/photos/57990a15437fcffe02f722fd/master/pass/traditional-napa-cabbage-kimchi.jpg"
     },
     {
+        id: 2,
         name: "Galbi",
         image: "https://c7.staticflickr.com/6/5531/30503823454_a75aae1f48_z.jpg"
     }
 ];
 
+// function renderFood(dish) {
+//     return <Food name={dish.name} picture={dish.image}/>
+// }
 
 function App() {
     return (
         <div>
+            {/*{foodsILike.map(renderFood)}*/}
             {foodsILike.map(dish => {
-                return <Food name={dish.name} picture={dish.image}/>
+                return <Food key={dish.id} name={dish.name} picture={dish.image}/>
             })}
         </div>
     );
