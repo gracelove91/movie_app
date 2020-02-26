dcfcd7dd55a72fb736ea496b9d1274494fa235ab
gracelove <govlmo91@gmail.com>
2020-02-27 01:53:23 +0900

    PropType으로 안전하게 보호하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 
-function Food({name, picture}) {
+function Food({name, picture, rating}) {
     return <div>
         <h2> I Like {name}</h2>
+        <h4>{rating}/5</h4>
         <img src={picture}  alt={name}/>
     </div>
 }
@@ -12,12 +14,14 @@ const foodsILike = [
     {
         id: 1,
         name: "Kimchi",
-        image: "http://assets.epicurious.com/photos/57990a15437fcffe02f722fd/master/pass/traditional-napa-cabbage-kimchi.jpg"
+        image: "http://assets.epicurious.com/photos/57990a15437fcffe02f722fd/master/pass/traditional-napa-cabbage-kimchi.jpg",
+        rating: 3.1
     },
     {
         id: 2,
         name: "Galbi",
-        image: "https://c7.staticflickr.com/6/5531/30503823454_a75aae1f48_z.jpg"
+        image: "https://c7.staticflickr.com/6/5531/30503823454_a75aae1f48_z.jpg",
+        rating: 4.5
     }
 ];
 
@@ -25,12 +29,22 @@ const foodsILike = [
 //     return <Food name={dish.name} picture={dish.image}/>
 // }
 
+Food.propTypes = {
+    name: propTypes.string.isRequired,
+    picture: propTypes.string.isRequired,
+    rating: propTypes.number.isRequired
+};
+
 function App() {
     return (
         <div>
             {/*{foodsILike.map(renderFood)}*/}
             {foodsILike.map(dish => {
-                return <Food key={dish.id} name={dish.name} picture={dish.image}/>
+                return <Food
+                    key={dish.id}
+                    name={dish.name}
+                    picture={dish.image}
+                    rating={dish.rating}/>
             })}
         </div>
     );
