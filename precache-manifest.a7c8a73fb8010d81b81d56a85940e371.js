36dadbc0677b1609e237fc63412f609f1aa1c35f
gracelove <govlmo91@gmail.com>
2020-02-27 23:01:27 +0900

    Updates

---
+++ precache-manifest.a7c8a73fb8010d81b81d56a85940e371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b8d02590ba961e845539e72f4dbd96",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ffaa4d2fc6f6b7d03c63",
+    "url": "/movie_app/static/css/main.7e23c686.chunk.css"
+  },
+  {
+    "revision": "580d5e9a24d6a17ca1b5",
+    "url": "/movie_app/static/js/2.8fcf6e80.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.8fcf6e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffaa4d2fc6f6b7d03c63",
+    "url": "/movie_app/static/js/main.0c7ef612.chunk.js"
+  },
+  {
+    "revision": "c0057b52e72b0f614c37",
+    "url": "/movie_app/static/js/runtime-main.15be7292.js"
+  }
+]);
